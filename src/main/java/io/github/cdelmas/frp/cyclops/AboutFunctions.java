41a95453ef8f8d505466f3be61bf68f7a08e40d8
cdelmas <cyril.delmas@gmail.com>
2017-11-05 13:16:13 +0100

    New Functions examples

--- src/main/java/io/github/cdelmas/frp/cyclops/AboutFunctions.java
+++ src/main/java/io/github/cdelmas/frp/cyclops/AboutFunctions.java
@@ -1,15 +1,47 @@
 package io.github.cdelmas.frp.cyclops;
 
+import com.aol.cyclops2.types.mixins.Printable;
+import cyclops.collections.mutable.SetX;
+import cyclops.function.FluentFunctions;
+import cyclops.function.FluentFunctions.FluentFunction;
+import cyclops.function.Fn1;
+import cyclops.function.Fn2;
+import cyclops.function.Lambda;
+import lombok.val;
+
+import java.util.function.Function;
+
+import static com.aol.cyclops2.types.mixins.AsMappable.asMappable;
+import static cyclops.function.Lambda.λ;
+
 public class AboutFunctions {
-    //Fn0-Fn8	Extended Function interfaces supporting map / flatMap / applicative operations, currying, partial application, lifting, composition and more
-    //C3-C5	Additional Consumers
-    //FluentFunctions	A fluent API for working with Functions - composition, lifting, AOP and more
-    //Lambda	An API for working with anomyous lambda expressions (type inferencing)
-    // => rewrite xor example with partial application
-    //Memoize	An API for caching pure functions
-    // => merge AboutMemoize
-    //PartialApplicator	An API for Partial Application of functions
-    //Curry / CurryConsumer / CurryVariance	An API for currying functions
-    //Monoid	A function for combining values of the same type, with an identity value
-    //Semigroup	A function for combining values of the same type
+
+    public static void main(String[] args) {
+        new AboutFunctions().fn();
+
+        new AboutFunctions().fluent();
+    }
+
+    private void fn() {
+        SetX.of(3, 4, 5, 6, 7, 12, 23)
+                .map(i -> Integer.sum(i, 3))
+                .forEach(i -> System.out.println(i));
+
+        Function<Integer,Integer> sumJ = λ(Integer::sum).apply(4);
+        // sumJ.
+
+        Fn1<Integer, Integer> sum = λ(Integer::sum).apply(4);
+        // sum.
+    }
+
+    private void fluent() {
+        FluentFunction<Integer, Integer> f = FluentFunctions.ofChecked((Integer x) -> {
+            if (x < 0) throw new IllegalArgumentException("x<0");
+            else return x + 1;
+        }).recover(IllegalArgumentException.class, i -> 42);
+
+        Integer res = f.apply(3); // 4
+        Integer universalAnswer = f.apply(-12); // 42
+    }
+
 }
